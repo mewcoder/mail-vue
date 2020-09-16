6cf40138821f4c10528e43957ba4761461aae9cc
mewcoder <1010953107@qq.com>
2020-09-16 23:13:21 +0800

    add vue页面和路由设置

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,73 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import Home from './../pages/home'
+import Index from './../pages/index'
+Vue.use(Router);
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
-})
-
-export default router
+export default new Router({
+    routes: [{
+            path: '/',
+            name: 'home',
+            component: Home,
+            redirect: '/index',
+            children: [{
+                path: '/index',
+                name: 'index',
+                component: Index,
+            }, {
+                path: '/product/:id',
+                name: 'product',
+                component: () =>
+                    import ('./../pages/product.vue')
+            }, {
+                path: '/detail/:id',
+                name: 'detail',
+                component: () =>
+                    import ('./../pages/detail.vue')
+            }]
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () =>
+                import ('./../pages/login.vue')
+        },
+        {
+            path: '/cart',
+            name: 'cart',
+            component: () =>
+                import ('./../pages/cart.vue')
+        },
+        {
+            path: '/order',
+            name: 'order',
+            component: () =>
+                import ('./../pages/order.vue'),
+            children: [{
+                    path: 'list',
+                    name: 'order-list',
+                    component: () =>
+                        import ('./../pages/orderList.vue')
+                },
+                {
+                    path: 'confirm',
+                    name: 'order-confirm',
+                    component: () =>
+                        import ('./../pages/orderConfirm.vue')
+                },
+                {
+                    path: 'pay',
+                    name: 'order-pay',
+                    component: () =>
+                        import ('./../pages/orderPay.vue')
+                },
+                {
+                    path: 'alipay',
+                    name: 'alipay',
+                    component: () =>
+                        import ('./../pages/alipay.vue')
+                }
+            ]
+        }
+    ]
+});
